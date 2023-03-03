3e91ff1ed6555f68e4c76192737888f4da6172c3
lizvdkamp <92380334+lizvdkamp@users.noreply.github.com>
2023-03-03 12:01:28 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Run handin Liz"
+
+
+echo "Download txt file for in report..."
+if [ ! -e Vandermonde.txt ]; then
+  wget home.strw.leidenuniv.nl/~daalen/Handin_files/Vandermonde.txt
+fi
+
+# Script that returns a txt file
+echo "Run the first script ..."
+python3 NURHW1LizQ1.py 
+
+# Script that pipes output to multiple files
+echo "Run the second script ..."
+python3 NURHW1LizQ2.py
+
+
+echo "Generating the pdf"
+
+pdflatex Solution.tex
+bibtex Solution.aux
+pdflatex Solution.tex
+pdflatex Solution.tex
